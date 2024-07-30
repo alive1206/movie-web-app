ee811fa1f9f4a5e18bdf94b45ddebfb43e48802a
Lê Bảo Thắng <thanglb.bi11-243@st.usth.edu.vn>
2024-07-30 15:32:46 +0700

    use pagination for genre

--- src/app/genre/[slug]/page.tsx
+++ src/app/genre/[slug]/page.tsx
@@ -1,6 +1,28 @@
 import { GenreComponent } from "@/containers";
+import axios from "axios";
 
-export default function GenreScreen({ params }: { params: { slug: string } }) {
+export default async function GenreScreen({
+  params,
+  searchParams,
+}: {
+  params: { slug: string };
+  searchParams: { [key: string]: string };
+}) {
   const { slug } = params;
-  return <GenreComponent slug={slug} />;
+
+  const fetchData = async (slug: string, page: number = 1) => {
+    try {
+      const res = await axios.get(
+        `https://phim.nguonc.com/api/films/the-loai/${slug}?page=${page}`
+      );
+
+      return res.data;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const page = searchParams?.page ? parseInt(searchParams.page) : 1;
+  const movies = await fetchData(slug, page);
+
+  return <GenreComponent slug={slug} data={movies} />;
 }
